3bf0726506c9455d2e9804c8a61203c1dfac62de
Bagus Nur Andiansyah <bagusnur@protonmail.com>
2024-01-16 09:33:27 +0800

    Chore: Setup for collab

---
+++ components/Loading.vue
@@ -0,0 +1,10 @@
+<template>
+  <!-- Loading pertama ketika page baru dibuka -->
+  <div>
+    <p>Loading</p>
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <Head>
+      <Title>Expo IF ITK 2024</Title>
+    </Head>
+
+    <!-- Loading Pertama-->
+    <Loading />
+
+    <!-- Index -->
+    <div>INdex</div>
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
